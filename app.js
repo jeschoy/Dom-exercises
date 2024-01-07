c406c18b424103266e698be4399f0969fe8f1592
Jessica Choi <jesymchoi@gmail.com>
2024-01-07 15:55:07 -0800

    exercises 6 through 8

--- app.js
+++ app.js
@@ -19,8 +19,15 @@ let containerDiv = document.querySelector('#container');
 containerDiv.innerText = "Hello!";
 
 // 6. Add the class main to the div with a class of footer.
+let footer = document.querySelector('.footer');
+footer.classList.add('main');
+
 // 7. Remove the class main on the div with a class of footer.
+footer.classList.remove('main');
+
 // 8. Create a new li element.
+let newLi = document.createElement('li');
+
 // 9. Give the li the text “four”.
 // 10. Append the li to the ul element.
 // 11. Loop over all of the lis inside the ol tag and give them a background color of “green”.
